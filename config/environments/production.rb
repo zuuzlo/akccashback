626ebadbca25e4922713c0eb6dfac4bcf51c04d9
Kirk Jarvis <zuuzlo@yahoo.com>
2014-10-15 21:22:11 -0500

    removed some files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Akccashback::Application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :dalli_store, nil, { :namespace => 'akccashback', :expires_in => 1.day, :compress => true }
+  config.cache_store = :dalli_store, { :namespace => 'akccashback', :expires_in => 1.day, :compress => true }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
