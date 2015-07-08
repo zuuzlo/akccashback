62e1b1f78223ff5c6d9b032fc593a864011cf615
Kirk Jarvis <zuuzlo@yahoo.com>
2015-07-07 22:27:59 -0500

    ads 4

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,6 +2,44 @@ require 'spec_helper'
 
 describe ApplicationController do
 
+  describe "current_user helper" do
+    let(:user1) { Fabricate(:user) }
+
+    context "no current_user" do
+      
+      it "returns nil" do
+        expect(controller.send(:current_user)).to be_nil
+      end
+    end
+
+    context "with current_user" do
+      it "set current_user to user1" do
+        set_current_user(user1)
+        expect(controller.send(:current_user)).to eq(user1)
+      end
+    end
+  end
+
+  describe "logged_in? helper" do
+    let(:user1) { Fabricate(:user) }
+
+    context "no current_user" do
+      
+      it "returns false" do
+        expect(controller.send(:logged_in?)).to be_false
+      end
+    end
+
+    context "with current_user" do
+
+      it "returns true" do
+        set_current_user(user1)
+        expect(controller.send(:logged_in?)).to be_true
+      end
+    end
+  end
+  
+
   describe "display_ad? helper" do
     let(:user1) { Fabricate(:user) }
     context "develoment" do
